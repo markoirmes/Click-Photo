82eeda807ec7b90f17ed6c089fafa25d0afbc422
Marko Nikolic <markoirmes@gmail.com>
2020-02-26 03:14:07 +0100

    Update site

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,22 +13,60 @@
 
   // Activate Feather icons
   feather.replace();
-
+/*
   // Activate Bootstrap scrollspy for the sticky nav component
   $("body").scrollspy({
     target: "#stickyNav",
-    offset: 82
+    offset: 96
   });
+*/
+/*
+// anchor on another page
+$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top -120
+    }, 500);
+    return false;
+}); */
+
+/*
+  // Smooth scrolling
+  $(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 120
+      }, 200, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+*/
+
 
   // Scrolls to an offset anchor when a sticky nav link is clicked
-  $('.nav-sticky a.nav-link[href*="#"]:not([href="#"])').click(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
       if (target.length) {
         $("html, body").animate(
           {
-            scrollTop: target.offset().top - 81
+            scrollTop: target.offset().top - 120
           },
           200
         );
@@ -37,6 +75,7 @@
     }
   });
 
+/*
   // Collapse Navbar
   // Add styling fallback for when a transparent background .navbar-marketing is scrolled
   var navbarCollapse = function() {
@@ -52,5 +91,5 @@
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  $(window).scroll(navbarCollapse); */
 })(jQuery);
